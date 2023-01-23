f95fe965134b73f6e2825e2af7c447638f1c9d4e
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-23 08:08:24 -0700

    Add project files.

---
+++ BetterCalculator/Program.vb
@@ -0,0 +1,13 @@
+'Tim Rossiter
+'RCET0265
+'Fall 2023
+'Joshua Makuch
+'
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("")
+        While 
+    End Sub
+End Module
