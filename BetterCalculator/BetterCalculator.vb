4a3c8e8918b36bfb3eabb0738cc5e29779755740
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-23 08:12:06 -0700

    Renamed the vb

---
+++ BetterCalculator/BetterCalculator.vb
@@ -0,0 +1,12 @@
+'Tim Rossiter
+'RCET0265
+'Fall 2023
+'Joshua Makuch
+'https://github.com/JoshuaMakuch/BetterCalculator.git
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Please enter two numbers. Enter {0}Q{0} at any time to quit.", Microsoft.VisualBasic.Chr(34))
+    End Sub
+End Module
