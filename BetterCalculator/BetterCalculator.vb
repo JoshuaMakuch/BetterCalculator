41701c28e5d7a9c3e9f825a1635d4a2f468eb70a
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-23 08:19:57 -0700

    Re-did the header and included the option in order to force decleration of variables (Explicit) and to prevent implicit narrowing conversions (Strict)

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -1,11 +1,15 @@
-'Tim Rossiter
+'Joshua Makuch
 'RCET0265
 'Fall 2023
-'Joshua Makuch
+'Better Calculator
 'https://github.com/JoshuaMakuch/BetterCalculator.git
+
+Option Strict On
+Option Explicit On
+
 Imports System
 
-Module Program
+Module BetterCalculator
     Sub Main(args As String())
         Console.WriteLine("Please enter two numbers. Enter {0}Q{0} at any time to quit.", Microsoft.VisualBasic.Chr(34))
     End Sub
