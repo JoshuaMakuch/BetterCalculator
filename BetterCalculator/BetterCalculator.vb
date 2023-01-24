3d38bdd6b63985946825671fc079c9ce4a583025
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-24 08:08:32 -0700

    Made sure that the gathering of user information works, on to the operator section

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -8,6 +8,8 @@ Option Strict On
 Option Explicit On
 
 Imports System
+Imports System.Net.Security
+Imports System.Numerics
 
 Module BetterCalculator
     Sub Main(args As String())
@@ -16,9 +18,12 @@ Module BetterCalculator
 
         Dim endProgram As Boolean = False
 
-        While endProgram
+        While endProgram = False
             endProgram = CalculatingTime()
         End While
+
+        Console.WriteLine("Have a nice day!")
+        Console.WriteLine("Press ENTER to close window.")
 
     End Sub
 
@@ -29,35 +34,42 @@ Module BetterCalculator
         Dim userInput2 As String
         Dim complete1 As Boolean = False
         Dim complete2 As Boolean = False
+
         While complete1 = False
             Console.WriteLine("Choose a first number")
             userInput1 = Console.ReadLine()
             If IsNumeric(userInput1) Then
+                Console.WriteLine($"You entered {userInput1}.")
                 num1 = CInt(userInput1)
                 Console.Write("Integer")
                 Console.WriteLine(num1 + 5)
                 complete1 = True
-            ElseIf userInput1 = "Q" Then
+            ElseIf userInput1.ToLower = "q" Then
                 Return True
             ElseIf IsNumeric(userInput1) = False Then
-                Console.WriteLine($"{userInput1} is not a numeric, please try again")
+                Console.WriteLine($"You entered {userInput1}, please enter a whole number.")
             End If
         End While
 
         While complete2 = False
             Console.WriteLine("Choose a second number")
-            userInput1 = Console.ReadLine()
+            userInput2 = Console.ReadLine()
             If IsNumeric(userInput2) Then
+                Console.WriteLine($"You entered {userInput2}.")
                 num2 = CInt(userInput2)
                 Console.Write("Integer")
                 Console.WriteLine(num2 + 5)
                 complete2 = True
-            ElseIf userInput2 = "Q" Then
+            ElseIf userInput2.ToLower = "q" Then
                 Return True
-            ElseIf IsNumeric(userInput1) = False Then
-                Console.WriteLine($"{userInput2} is not a numeric, please try again")
+            ElseIf IsNumeric(userInput2) = False Then
+                Console.WriteLine($"You entered {userInput2}, please enter a whole number.")
             End If
         End While
+
+        If complete1 And complete2 Then
+            Console.WriteLine("Choose one of the following options:")
+        End If
 
         Return False
     End Function
