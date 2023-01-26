cad3dddcb77840130f6b970f203218b641eadda0
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-26 08:57:25 -0700

    Removed a few unecessary variables and lines and swapped the function for a sub that contains the forever loop within it. Also added a few comments and spaced things out to be able to be read a little better.

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -18,12 +18,7 @@ Module BetterCalculator
         Console.WriteLine("") 'Any empty write lines are for spacing in the console
         Console.WriteLine("Please enter two numbers. Enter {0}Q{0} at any time to quit.", Microsoft.VisualBasic.Chr(34))
 
-        Dim endProgram As Boolean = False
-
-        'Loop until the user decides to quit
-        While endProgram = False
-            endProgram = CalculatingTime()
-        End While
+        CalculatingTime() 'Loops until the user presses q and exits the program
 
         Console.WriteLine("")
         Console.WriteLine("Have a nice day!")
@@ -31,83 +26,93 @@ Module BetterCalculator
 
     End Sub
 
-    Function CalculatingTime() As Boolean
-        Dim num1 As Double 'This ensures that the limit for the number you input isnt going to crash the code
+    Sub CalculatingTime()
+
+        'This ensures that the limit for the number you input isnt going to crash the code
+        Dim num1 As Double
         Dim num2 As Double
         Dim userInput As String
-        Dim complete1 As Boolean = False
-        Dim complete2 As Boolean = False
-        Dim complete3 As Boolean = False
+        Dim complete As Boolean = False
 
-        'This section obtains the first number from the user 
-        While complete1 = False
-            Console.WriteLine("")
-            Console.WriteLine("Choose a first number")
-            userInput = Console.ReadLine()
-            If IsNumeric(userInput) Then
-                Console.WriteLine($"You entered {CDbl(userInput)}.")
-                num1 = CDbl(userInput)
-                complete1 = True
-            ElseIf userInput.ToLower = "q" Then
-                Return True
-            ElseIf IsNumeric(userInput) = False Then
-                Console.WriteLine($"You entered {userInput}, please enter a number.")
-            End If
-        End While
+        While True
 
-        'This section obtains the second number from the user 
-        While complete2 = False
-            Console.WriteLine("")
-            Console.WriteLine("Choose a second number")
-            userInput = Console.ReadLine()
-            If IsNumeric(userInput) Then
-                Console.WriteLine($"You entered {CDbl(userInput)}.")
-                num2 = CDbl(userInput)
-                complete2 = True
-            ElseIf userInput.ToLower = "q" Then
-                Return True
-            ElseIf IsNumeric(userInput) = False Then
-                Console.WriteLine($"You entered {userInput}, please enter a number.")
-            End If
-        End While
+            'This section obtains the first number from the user
+            While complete = False
+                Console.WriteLine("")
+                Console.WriteLine("Choose a first number")
+                userInput = Console.ReadLine()
+                If IsNumeric(userInput) Then
+                    Console.WriteLine($"You entered {CDbl(userInput)}.")
+                    num1 = CDbl(userInput)
+                    complete = True
+                ElseIf userInput.ToLower = "q" Then
+                    Exit Sub
+                ElseIf IsNumeric(userInput) = False Then
+                    Console.WriteLine($"You entered {userInput}, please enter a number.")
+                End If
+            End While
+            complete = False
 
-        'This section does the calculation of the two previously obtained numbers
-        While complete3 = False
-            Console.WriteLine("")
-            Console.WriteLine("Choose one of the following options:")
-            'Could put  "" & vbNewLine & "" to get rid of all console.writeline,
-            'but it makes the lines really long
-            Console.WriteLine("1. Add")
-            Console.WriteLine("2. Subtract")
-            Console.WriteLine("3. Multiply")
-            Console.WriteLine("4. Divide")
-            userInput = Console.ReadLine()
-            If IsNumeric(userInput) Then
-                'userInput = Dbl(userInput) doesn't work due to option explicit...
-                Console.WriteLine($"You entered {userInput}.")
-                If CDbl(userInput) = 1 Then
-                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}") 'Add
-                    complete3 = True
-                ElseIf CDbl(userInput) = 2 Then
-                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}") 'Sub
-                    complete3 = True
-                ElseIf CDbl(userInput) = 3 Then
-                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}") 'Multiply
-                    complete3 = True
-                ElseIf CDbl(userInput) = 4 Then
-                    Console.WriteLine($"{num1} / {num2} = {num1 / num2}") ' Divide
-                    complete3 = True
-                ElseIf CDbl(userInput) <> CDbl(1 & 2 & 3 & 4) Then
+            'This section obtains the second number from the user 
+            While complete = False
+                Console.WriteLine("")
+                Console.WriteLine("Choose a second number")
+                userInput = Console.ReadLine()
+                If IsNumeric(userInput) Then
+                    Console.WriteLine($"You entered {CDbl(userInput)}.")
+                    num2 = CDbl(userInput)
+                    complete = True
+                ElseIf userInput.ToLower = "q" Then
+                    Exit Sub
+                ElseIf IsNumeric(userInput) = False Then
+                    Console.WriteLine($"You entered {userInput}, please enter a number.")
+                End If
+            End While
+            complete = False
+
+            'This section does the calculation of the two previously obtained numbers
+            While complete = False
+                Console.WriteLine("")
+                Console.WriteLine("Choose one of the following options:")
+                'Could put  "" & vbNewLine & "" to get rid of all console.writeline,
+                'but it makes the line really long
+                Console.WriteLine("1. Add")
+                Console.WriteLine("2. Subtract")
+                Console.WriteLine("3. Multiply")
+                Console.WriteLine("4. Divide")
+                userInput = Console.ReadLine()
+                If IsNumeric(userInput) Then
+
+                    'userInput = Dbl(userInput) doesn't work due to option explicit...
+                    Console.WriteLine($"You entered {userInput}.")
+                    If CDbl(userInput) = 1 Then
+                        Console.WriteLine($"{num1} + {num2} = {num1 + num2}") 'Add
+                        complete = True
+                    ElseIf CDbl(userInput) = 2 Then
+                        Console.WriteLine($"{num1} - {num2} = {num1 - num2}") 'Subtract
+                        complete = True
+                    ElseIf CDbl(userInput) = 3 Then
+                        Console.WriteLine($"{num1} * {num2} = {num1 * num2}") 'Multiply
+                        complete = True
+                    ElseIf CDbl(userInput) = 4 Then
+                        Console.WriteLine($"{num1} / {num2} = {num1 / num2}") 'Divide
+                        complete = True
+                    ElseIf CDbl(userInput) <> CDbl(1 & 2 & 3 & 4) Then
+                        Console.WriteLine($"{userInput} is an invalid option, please enter a correct option.")
+                    End If
+
+                ElseIf userInput.ToLower = "q" Then
+                    Exit Sub
+                ElseIf IsNumeric(userInput) = False Then
                     Console.WriteLine($"{userInput} is an invalid option, please enter a correct option.")
                 End If
-            ElseIf userInput.ToLower = "q" Then
-                Return True
-            ElseIf IsNumeric(userInput) = False Then
-                Console.WriteLine($"{userInput} is an invalid option, please enter a correct option.")
-            End If
+
+            End While
+            complete = False
 
         End While
-        Return False
-    End Function
+
+    End Sub
 
 End Module
+
